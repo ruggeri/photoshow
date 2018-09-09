008245dc86b2cbacdfc5ac8f34627b874e76d3ad
Ned Ruggeri <ruggeri@self-loop.com>
2018-09-08 17:08:09 -0700

    add data-json-url attribute

--- src/index.js
+++ src/index.js
@@ -123,11 +123,14 @@ class PhotoshowController {
 }
 
 function initializePhotoshow() {
+  const photoshowEl = document.getElementById("photoshow");
   const imgEl = document.getElementById("live-photo-img");
   const videoEl = document.getElementById("live-photo-video");
   const commentEl = document.getElementById("live-photo-comment");
 
-  fetch("./data.json")
+  const dataUrl = photoshowEl.dataset["jsonUrl"];
+
+  fetch(dataUrl)
     .then((response) => response.json())
     .then((photoshowEntries) => {
       const livePhotoController = new LivePhotoController({
